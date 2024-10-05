34a55450449f1d89b9bf32daf1605ca4e7497630
SupraTheGreat <94575472+SupraTheGreat@users.noreply.github.com>
2024-10-05 14:55:52 -0500

    v2 MecanumTeleOp.java

--- Tutorial/MecanumTeleOp.java
+++ Tutorial/MecanumTeleOp.java
@@ -11,39 +11,39 @@ public class MecanumTeleOp extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // Declare our motors
         // Make sure your ID's match your configuration
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("Left Front Motor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("Left Back Motor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("Right Front Motor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("Right Back Motor");
 
         // Reverse the right side motors. This may be wrong for your setup.
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
         // See the note about this earlier on this page.
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE); // Remove if needed
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE); // Remove if needed
 
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
+            double drive /*Front and Back*/ = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double strafe /*Left to Right*/ = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double twist /*Turning*/ = gamepad1.right_stick_x; // This is the RIGHT stick, indpendent of drive and strafe
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1
+            double frontLeftPower = (drive + strafe + twist) / denominator; // ++
+            double backLeftPower = (drive - strafe + twist) / denominator; //-+
+            double frontRightPower = (drive - strafe - twist) / denominator; //--
+            double backRightPower = (drive + strafe - twist) / denominator; // +-
 
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
+            frontRightMotor.setPower(frontRightPower);   
             backRightMotor.setPower(backRightPower);
         }
     }
