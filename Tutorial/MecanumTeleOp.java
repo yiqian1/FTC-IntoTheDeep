b7e92c8c4d4e5f3dfa64763518285b3c75bdf042
SupraTheGreat <94575472+SupraTheGreat@users.noreply.github.com>
2024-10-12 15:27:05 -0500

    fix MecanumTeleOp.java

--- Tutorial/MecanumTeleOp.java
+++ Tutorial/MecanumTeleOp.java
@@ -20,35 +20,37 @@ public class MecanumTeleOp extends LinearOpMode {
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
         // See the note about this earlier on this page.
-        // frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE); // Remove if needed
-        // backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE); // Remove if needed
+        // frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        // backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
         telemetry.addData("Start",1);
         
         if (isStopRequested()) return;
-
+        
         while (opModeIsActive()) {
             double drive /*Front and Back*/ = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double strafe /*Left to Right*/ = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double twist /*Turning*/ = -gamepad1.right_stick_x; // This is the RIGHT stick, indpendent of drive and strafe
-            telemetry.addData("drive", drive);
-            telemetry.addData("strafe", strafe);
-            telemetry.addData("twist", twist);
+            double twist /*Turning*/ = gamepad1.right_stick_x; // This is the RIGHT stick, indpendent of drive and strafe
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(drive) + Math.abs(strafe) + Math.abs(twist), 1);
             double frontLeftPower = (drive + strafe + twist) / denominator; // ++
-            double frontRightPower = (drive - strafe + twist) / denominator; //-+
-            double backLeftPower = (drive - strafe - twist) / denominator; //--
+            double frontRightPower = (drive - strafe + twist) / denominator; //--
+            double backLeftPower = (drive - strafe - twist) / denominator; //-+
             double backRightPower = (drive + strafe - twist) / denominator; // +-
 
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);   
+            frontLeftMotor.setPower(-frontLeftPower); // Reversed
+            backLeftMotor.setPower(-backLeftPower); // Reversed
+            frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
+            
+            // frontLeftMotor.setPower(0); // Backwards
+            // backLeftMotor.setPower(0); // Backwards
+            // frontRightMotor.setPower(0); // Forwards
+            // backRightMotor.setPower(0); // Forwards
         }
     }
 }
