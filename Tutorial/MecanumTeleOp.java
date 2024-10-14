d734aff98a5018f20cc6658ae2e95157cba4a634
SupraTheGreat <94575472+SupraTheGreat@users.noreply.github.com>
2024-10-14 13:36:46 -0500

    Comment update

--- Tutorial/MecanumTeleOp.java
+++ Tutorial/MecanumTeleOp.java
@@ -9,29 +9,25 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 public class MecanumTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        // Declare our motors
-        // Make sure your ID's match your configuration
+        // Declaring our motors
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("Left Front Motor");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("Left Back Motor");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("Right Front Motor");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("Right Back Motor");
-        telemetry.addData("Intialize",1);
-        // Reverse the right side motors. This may be wrong for your setup.
-        // If your robot moves backwards when commanded to go forwards,
-        // reverse the left side instead.
-        // See the note about this earlier on this page.
+        telemetry.addData("Intialize",1); // Test code, to make sure telemetry works
+
         // frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         // backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
-        telemetry.addData("Start",1);
+        telemetry.addData("Start",1); // Test code again
         
         if (isStopRequested()) return;
         
         while (opModeIsActive()) {
-            double drive /*Front and Back*/ = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double drive /*Front and Back*/ = -gamepad1.left_stick_y; // Y stick value is reversed
             double strafe /*Left to Right*/ = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double twist /*Turning*/ = gamepad1.right_stick_x; // This is the RIGHT stick, indpendent of drive and strafe
+            double twist /*Turning*/ = gamepad1.right_stick_x; // This is the RIGHT stick, in a way indpendent of drive and strafe
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
